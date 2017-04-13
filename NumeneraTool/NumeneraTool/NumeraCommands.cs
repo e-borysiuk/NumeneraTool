a832b2a19bca8e119704212f28306256116b71cd
Emil <Emil@DESKTOP-Q00BH9Q>
2017-04-14 00:37:31 +0200

    Created interface and basic commands

---
+++ NumeneraTool/NumeneraTool/NumeraCommands.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Input;
+
+namespace NumeneraTool
+{
+    public class NumeraCommands
+    {
+        private static RoutedUICommand _addValue;
+        private static RoutedUICommand _subtractValue;
+
+
+        static NumeraCommands()
+        {
+            _addValue = new RoutedUICommand(
+                "Add", "Adds value", typeof(NumeraCommands));
+            _subtractValue = new RoutedUICommand(
+                "Subtract", "Subtracts value", typeof(NumeraCommands));
+        }
+
+        public static RoutedUICommand AddValue
+        {
+            get { return _addValue; }
+        }
+
+        public static RoutedUICommand SubtractValue
+        {
+            get { return _subtractValue; }
+        }
+    }
+}
