447fb882939bc8a539cd661411de19e0886b9d0a
Emil <Emil@DESKTOP-Q00BH9Q>
2017-04-17 00:58:24 +0200

    Created Player and Data classes, imporved interface, moved styles to seprate file.

---
+++ NumeneraTool/NumeneraTool/DataClass.cs
@@ -0,0 +1,18 @@
+﻿namespace NumeneraTool
+{
+    public class DataClass
+    {
+        public DataClass()
+        {
+            Current = 0;
+            Pool = 0;
+            Edge = 0;
+        }
+
+        public short Current { get; set; }
+
+        public short Pool { get; set; }
+
+        public short Edge { get; set; }
+    }
+}
---
+++ NumeneraTool/NumeneraTool/Player.cs
@@ -0,0 +1,16 @@
+﻿namespace NumeneraTool
+{
+    public class Player
+    {
+        public DataClass Might { get; set; }
+        public DataClass Speed { get; set; }
+        public DataClass Intellect { get; set; }
+        public string Name { get; set; }
+        public Player()
+        {
+            Might = new DataClass();
+            Speed = new DataClass();
+            Intellect = new DataClass();
+        }
+    }
+}
